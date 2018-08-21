54b750d1bc2dca5260966c6b15c1bb731fa81588
TheErrorGuy <lavan.puri99@yahoo.com>
2018-08-21 22:07:04 +0530

    added geolocation final

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,16 @@ socket.on('newMessage',function(message){
   li.text(`${message.from}: ${message.text}`)
   $('#messages').append(li);
 });
+
+socket.on('newLocationMessage',function(message){
+  var li = $('<li></li>');
+  var a= $('<a target="_blank">My current location</a>');
+  li.text(`${message.from}`);
+  a.attr('href',message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit',function(e){
   e.preventDefault();
   socket.emit('createMessage',{
