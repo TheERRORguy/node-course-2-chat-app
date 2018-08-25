11e8d9a21ca2895940957e3339386c68f2d6d792
TheErrorGuy <lavan.puri99@yahoo.com>
2018-08-25 13:41:27 +0530

    added mustache rendering

--- public/js/index.js
+++ public/js/index.js
@@ -6,20 +6,25 @@ socket.on('disconnect',function(){
   console.log("disconnected");
 });
 socket.on('newMessage',function(message){
+  var template = $('#message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
-  $('#messages').append(li);
+  var html = Mustache.render(template,{
+    text : message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
+  var template = $('#location-message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a= $('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime} `);
-  a.attr('href',message.url);
-  li.append(a);
-  $('#messages').append(li);
+  var html = Mustache.render(template,{
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+    $('#messages').append(html);
 });
 
 var messageTextbox = $('[name=message]');
